51b908bc6c36ccc28e0513ece5f35628af20c8ef
Diandra2 <diandraedwigesamaral@gmail.com>
2022-04-05 08:41:35 -0300

    05 de abril

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+// auto load
+spl_autoload_extensions('.php');
+function classLoader($class)
+{
+  $nomeArquivo = $class . '.php';
+  $pastas = array('controller', 'model');
+  foreach ($pastas as $pasta) {
+    $arquivo = "{$pasta}/{$nomeArquivo}";
+    if (file_exists($arquivo)) {
+      require_once($arquivo);
+    }
+  }
+}
+spl_autoload_register('classLoader');
+// Front Controller
+class Aplicacao
+{
+  public static function run()
+  {
+    $layout = new Template('view/layout.html');
+    $class = "Inicio";
+    if (class_exists($class)) {
+      $pagina = new $class();
+      $conteudo = $pagina->controller();
+      $layout->set('conteudo', $conteudo);
+    }
+    echo $layout->saida();
+  }
+}
+Aplicacao::run();
