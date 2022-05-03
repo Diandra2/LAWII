26a76f3587ba9581dd1f0ceaafd8b1cb38e30958
Diandra2 <diandraedwigesamaral@gmail.com>
2022-05-03 07:46:37 -0300

    aula do dia 03 de maio

--- controller/Form.php
+++ controller/Form.php
@@ -9,6 +9,10 @@ class Form
   public function controller()
   {
     $form = new Template("view/form.html");
+    $form->set("id", "");
+    $form->set("disciplina", "");
+    $form->set("professor", "");
+    $form->set("materia", "");
     $this->message = $form->saida();
   }
   public function salvar()
@@ -16,11 +20,40 @@ class Form
     if (isset($_POST["disciplina"]) && isset($_POST["professor"]) && isset($_POST["materia"])) {
       try {
         $conexao = Transaction::get();
-        $avaliacao = new Crud("avaliacao ");
+        $avaliacao = new Crud("avaliacao");
         $disciplina = $conexao->quote($_POST["disciplina"]);
-        $professor = $conexao->quote($_POST["professor"]);
+        $config = $conexao->quote($_POST["professor"]);
         $materia = $conexao->quote($_POST["materia"]);
-        $resultado = $avaliacao->insert("disciplina, professor, materia", "$disciplina, $professor, $materia");
+        if (empty($_POST["id"])) {
+          $avaliacao->insert(
+            "disciplina, professor, materia",
+            "$disciplina, $config, $materia"
+          );
+        } else {
+          $id = $conexao->quote($_POST["id"]);
+          $avaliacao->update(
+            "disciplina = $disciplina, professor = $config, materia = $materia",
+            "id = $id"
+          );
+        }
+      } catch (Exception $e) {
+        echo $e->getMessage();
+      }
+    }
+  }
+  public function editar()
+  {
+    if (isset($_GET["id"])) {
+      try {
+        $conexao = Transaction::get();
+        $id = $conexao->quote($_GET["id"]);
+        $avaliacao = new Crud("avaliacao");
+        $resultado = $avaliacao->select("*", "id = $id");
+        $form = new Template("view/form.html");
+        foreach ($resultado[0] as $cod => $materia) {
+          $form->set($cod, $materia);
+        }
+        $this->message = $form->saida();
       } catch (Exception $e) {
         echo $e->getMessage();
       }
